63068057be014096900a29df1c9561e1c7ee9972
Matthew Promenchenkel <mattprom@gmail.com>
2020-06-16 15:09:12 -0400

    randomly pick up to 4 gearlocs

--- script.js
+++ script.js
@@ -2,13 +2,23 @@
 // Fill out player names - select Ghille if playerName = Geoff
 // Choose how many players we are randomly selecting for
 
-
 window.onload = function () {
-const gearlocList = ["Picket","Patches","Tantrum","Boomer","Tink","Ghille","Nugget","Stanza","Duster","Gasket","Lab Rats","Dart"]
+var gearlocList = ["Picket","Patches","Tantrum","Boomer","Tink","Ghille","Nugget","Stanza","Duster","Gasket","Lab Rats","Dart"]
 const randomGearloc = document.querySelector('#randomGearloc')
+var clicks = 0;
 randomGearloc.addEventListener('click', (e) => {   
-    var loc = document.getElementById('selectedGearloc');
-    loc.innerHTML = gearlocList[Math.floor(Math.random() * 12)]
+   clicks += 1; 
+   if (clicks > 4){
+    document.getElementById('Gearloc1').innerHTML = "";
+    document.getElementById('Gearloc2').innerHTML = "";
+    document.getElementById('Gearloc3').innerHTML = "";
+    document.getElementById('Gearloc4').innerHTML = "";
+    clicks = 1;
+  } 
+    var loc = document.getElementById('Gearloc' + clicks);
+    var randNum = Math.floor(Math.random() * (gearlocList.length - 1));
+    loc.innerHTML = gearlocList[randNum];
+    gearlocList.splice(randNum,1);
 })
 
 }
